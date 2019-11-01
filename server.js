88ae22698a63ef4d77463407b8c37142271752e3
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-11-01 16:47:34 +0100

    Add files

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+app.use('/store', function(req, res, next){
+    console.log('Jestem pośrednikiem przy żądaniu do /store');
+    next();
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello world!');
+});
+
+app.get('/store', function (req, res) {
+    res.send('To jest sklep');
+});
+
+app.get('/first-template', function(req, res){
+    res.render('first-template');
+});
+
+app.get('/login', function(req, res){
+    res.render('log-in');
+});
+
+app.get('/auth/google', function(req, res){
+    res.render('welcome', {
+        user: {
+            name: req.query.name
+        }
+    });
+});
+
+app.listen(3000);
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
